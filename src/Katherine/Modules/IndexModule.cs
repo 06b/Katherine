58e9b33b7d7005195b6083a3f65dbec053215310
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-01-22 00:01:05 -0500

    Created a new Nancy with ASP.NET host with Razor Project

---
+++ src/Katherine/Modules/IndexModule.cs
@@ -0,0 +1,15 @@
+﻿using Nancy;
+
+namespace Katherine.Modules
+{
+    public class IndexModule : NancyModule
+    {
+        public IndexModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["index"];
+            };
+        }
+    }
+}
