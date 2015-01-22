09a63c131029ef8623ac42a48f1b9342d7be1d64
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-01-22 13:49:33 -0500

    Added Elmah for exception logging

--- src/Katherine/Bootstrapper.cs
+++ src/Katherine/Bootstrapper.cs
@@ -1,4 +1,7 @@
 ﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Elmah;
+using Nancy.TinyIoc;
 
 namespace Katherine
 {
@@ -7,5 +10,21 @@ namespace Katherine
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+
+            base.ApplicationStartup(container, pipelines);
+
+            /// <summary>
+            /// Elmah Logging - Enable Exception logging and select HttpStatusCode logging
+            /// </summary>
+
+            //TODO: Secure Elmah
+
+            Elmahlogging.Enable(pipelines, "elmah", null, new HttpStatusCode[] { HttpStatusCode.NotFound, HttpStatusCode.InsufficientStorage, });
+
+        }
     }
 }
