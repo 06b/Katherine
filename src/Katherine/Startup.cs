13edc5eb68f84dfd14a4d8ed5cef798cac7203db
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-02-15 22:53:01 -0500

    Added OWIN startup file

---
+++ src/Katherine/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Katherine.Startup))]
+
+namespace Katherine
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy();
+        }
+    }
+}
