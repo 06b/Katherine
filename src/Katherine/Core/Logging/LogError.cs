8e08e9360d9190802f207fcc5bec3701d8ef6ca3
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-02-19 16:22:48 -0500

    Make it possible to manually log errors

---
+++ src/Katherine/Core/Logging/LogError.cs
@@ -0,0 +1,39 @@
+﻿using Elmah;
+using System;
+using System.Web;
+
+namespace Katherine.Core.Logging
+{
+    public static class ErrorLog
+    {
+
+        /// <summary>
+        /// Manually Log errors to Elmah
+        /// Source: http://stackoverflow.com/a/14770142
+        /// </summary>
+        public static void LogError(Exception ex, string contextualMessage = null)
+        {
+            try
+            {
+                // log error to Elmah
+                if (contextualMessage != null)
+                {
+                    // log exception with contextual information that's visible when
+                    // clicking on the error in the Elmah log
+                    var annotatedException = new Exception(contextualMessage, ex);
+                    ErrorSignal.FromCurrentContext().Raise(annotatedException, HttpContext.Current);
+                }
+                else
+                {
+                    ErrorSignal.FromCurrentContext().Raise(ex, HttpContext.Current);
+                }
+
+            }
+            catch (Exception)
+            {
+                // uh oh! just keep going
+                // The caveat is a third party API could keep calling itself and blow up the app pool
+            }
+        }
+    }
+}
