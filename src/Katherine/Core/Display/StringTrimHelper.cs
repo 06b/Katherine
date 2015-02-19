b0eaaa8f97e45666bf8ef19ff7eb2dbef3d458c3
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-02-19 16:58:33 -0500

    Add String Trim Helper

---
+++ src/Katherine/Core/Display/StringTrimHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Katherine.Core.Display
+{
+    /// <summary>
+    /// Helper class used for trimming strings up to a certain chartacter limit and adds an ellipsis afterwards if requested
+    /// </summary>
+    public class StringTrimHelper
+    {
+        /// <summary>
+        /// Trims the string.
+        /// </summary>
+        /// <param name="Str">The string.</param>
+        /// <param name="CharacterLimit">The character limit.</param>
+        /// <returns></returns>
+        public static string TrimString(object Str, object CharacterLimit)
+        {
+            return TrimString(Str.ToString(), Convert.ToInt32(CharacterLimit));
+        }
+
+        /// <summary>
+        /// Trims the string.
+        /// </summary>
+        /// <param name="Str">The string.</param>
+        /// <param name="CharacterLimit">The character limit.</param>
+        /// <param name="AddEllipsis">if set to <c>true</c> [add ellipsis].</param>
+        /// <returns></returns>
+        public static string TrimString(string Str, int CharacterLimit, bool AddEllipsis)
+        {
+            if (Str.Length > CharacterLimit)
+            {
+                if (AddEllipsis) { return Str.Substring(0, CharacterLimit) + "..."; }
+                else
+                {
+                    return Str.Substring(0, CharacterLimit);
+                }
+            }
+
+            return Str;
+        }
+    }
+}
