5404eb9e7e30986bb2bfe2ea12bdda2b43d487e1
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-02-20 15:29:25 -0500

    Check to see if ExpandoObject contains a definition

---
+++ src/Katherine/Core/Extensions/ExpandoObjectExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+
+namespace Katherine.Core.Extensions
+{
+    /// <summary>
+    /// Check to see if ExpandoObject contains a definition.
+    /// Source: http://stackoverflow.com/a/11738271
+    /// </summary>
+    public static class ExpandoObjectExtensions
+    {
+        public static bool HasValue(this object val, string key)
+        {
+            var expando = val as ExpandoObject;
+            if (expando == null) return false;
+
+            return ((IDictionary<string, object>)expando).ContainsKey(key);
+        }
+    }
+}
