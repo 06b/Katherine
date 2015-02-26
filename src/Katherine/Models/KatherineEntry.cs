6961a07cc7a724158aae21883f764f0b6857b87b
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-02-26 10:20:51 -0500

    Add Entry & Route Models
    By splitting Entries & Routes this should allow for keeping track of
    changes - Takes care of half of #8

---
+++ src/Katherine/Models/KatherineEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Katherine.Models
+{
+    public class KatherineEntry
+    {
+        public Guid RouteId { get; set; }
+        public Guid ContentId { get; set; } /* Section Id */
+
+        public string PostName { get; set; }
+        public string PostTagLine { get; set; }
+        public string PostContent { get; set; }
+        public Guid PostId { get; set; }
+
+        public string MetaTitle { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaKeywords { get; set; }
+
+
+        public DateTime CreateDate { get; set; }
+        public Guid CreateBy { get; set; }
+        public string ViewName { get; set; }
+
+        public string PostThumbnail { get; set; }
+
+        public bool IsGatedAsset { get; set; }
+        public string PostAsset { get; set; }
+
+    }
+
+}
---
+++ src/Katherine/Models/KatherineRoute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Katherine.Models
+{
+    public class KatherineRoute
+    {
+
+        public Guid RouteId { get; set; }
+        public string Route { get; set; } /* Section Id */
+
+        public bool IsPublished { get; set; }
+        public DateTime? IsDeleted { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public Guid CreateBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+        public Guid? ModifiedBy { get; set; }
+
+        public Guid ContentTypeId { get; set; } /* Section Id */
+        public Guid ContentId { get; set; } /* Id of the custom content whatever it may be */
+
+
+    }
+}
