86a1431c58a5b9dce572889e0c7fba3a9a46b73f
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-04-06 13:42:46 -0400

    Added User Model

---
+++ src/Katherine/Models/KatherineUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Security;
+
+namespace Katherine.Models
+{
+    public class KatherineUser : IUserIdentity
+    {
+        public Guid UserID { get; set; }
+
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public IEnumerable<string> Claims { get; set; }
+    }
+}
