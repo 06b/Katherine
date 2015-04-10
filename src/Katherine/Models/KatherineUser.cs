17b9b6227e4f8464854600b9855e7588bce2bee3
Adrian D. Alvarez <adrian@adriandalvarez.com>
2015-04-09 23:41:12 -0400

    Updated KatherineUser Model

--- src/Katherine/Models/KatherineUser.cs
+++ src/Katherine/Models/KatherineUser.cs
@@ -9,8 +9,33 @@ namespace Katherine.Models
         public Guid UserID { get; set; }
 
         public string UserName { get; set; }
+
+        // Maximum length of a valid email address is 254 characters.
+        // See Dominic Sayers answer at SO: http://stackoverflow.com/a/574698/99240
         public string Email { get; set; }
+        public bool IsAccountVerified { get; set; }
+
         public string Password { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime LastLogin { get; set; }
+
+        public bool RequiresPasswordReset { get; set; }
+        public DateTime LastPasswordChangeDate { get; set; }
+
+        public DateTime PasswordResetRequestedDate { get; set; }
+
+        public bool LockoutEnabled{ get; set; }
+        public DateTime LockoutEndDate { get; set; }
+
+        public int FailedAttemptsCount { get; set; }
+        public DateTime LastFailedLogin { get; set; }
+
+        public string LastLockoutReason { get; set; }
+
+        public bool IsAccountClosed { get; set; }
+        public DateTime AccountClosedDate { get; set; }
 
         public IEnumerable<string> Claims { get; set; }
     }
